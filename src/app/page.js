54b79a47700bd50bac00b8da1c4ba5bdab6ba6f6
Robin van Doorn <robinmvdoorn@gmail.com>
2025-04-01 14:15:40 +0200

    chore: remove spacing

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
-import Countdown from "./ui/Countdown";
+import Countdown from "../../ui/countdown";
 import Accordion from "../../ui/faq.jsx";
-
 export default function Home() {
   return (
-   <div>
-      <Countdown />
-   </div>
     <div>
-      <Accordion />
+      <Countdown />
+      <div>
+        <Accordion />
+      </div>
     </div>
   );
 }
