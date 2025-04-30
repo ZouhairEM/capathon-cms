1ae1d8b27dda25d819e88e5f34f3c7f9fc242301
Mynorka Daza <mynorkadaza@gmail.com>
2025-04-30 11:30:08 +0200

    chore: update Button

--- src/app/ui/SignupSection.jsx
+++ src/app/ui/SignupSection.jsx
@@ -2,7 +2,6 @@
 import { Button } from "primereact/button";
 
 export default function SignupSection({ data }) {
-
   function handleSignUp() {
     console.log("Clicked!");
   }
@@ -10,19 +9,11 @@ export default function SignupSection({ data }) {
   return (
     <section className="text-center">
       <p className="w-2/3 m-auto mb-4">{data.content}</p>
-      {data.signUpPossible ? (
-        <Button 
-          label={data.buttonLabels.label1} 
-          onClick={handleSignUp} 
+        <Button
+          label={data.signUpPossible ? data.buttonLabels.label1 : data.buttonLabels.label2}
+          onClick={handleSignUp}
           disabled={!data.signUpPossible}
         ></Button>
-      ) : (
-        <Button 
-          label={data.buttonLabels.label2} 
-          onClick={handleSignUp} 
-          disabled={!data.signUpPossible}
-        ></Button>
-      )}
     </section>
   );
 }
