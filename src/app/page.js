d7a8e9a9d6597ce6a0424c613c4b768ca49f17f7
Robin van Doorn <robinmvdoorn@gmail.com>
2025-05-02 13:19:57 +0200

    feature: add organiser section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Countdown from "./ui/Countdown";
 import FaqSection from "./ui/FaqSection.jsx";
 import { getData } from "./service/FetchData";
 import Navbar from "./ui/Navbar";
+import OrganiserSection from "./ui/OrganiserSection";
 
 export default async function Home() {
   const data = await getData();
@@ -13,6 +14,7 @@ export default async function Home() {
       <Navbar data={navbar} />
       <FaqSection />
       <Countdown data={countdownData.countdown} />
+      <OrganiserSection />
     </div>
   );
 }
