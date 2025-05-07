fa9b5470072dbf1296a99bc20055fe9bf8e32530
Mynorka Daza <mynorkadaza@gmail.com>
2025-05-07 14:07:02 +0200

    wip

--- src/app/ui/HeroSection.jsx
+++ src/app/ui/HeroSection.jsx
@@ -27,8 +27,15 @@ export default function HeroSection({ data }) {
 
       <div className="border border-blue-800 flex flex-row justify-between py-10">
         {data.highlights.map((items) => (
-          <div key={items.text} className="flex-1 flex flex-col items-center gap-5">
-            <img src={items.icon} alt="icon" className="w-9 h-9 sm:w-16 sm:h-16" />
+          <div
+            key={items.text}
+            className="flex-1 flex flex-col items-center gap-5"
+          >
+            <img
+              src={items.icon}
+              alt="icon"
+              className="w-9 h-9 sm:w-16 sm:h-16"
+            />
             <p className="text-center text-sm">{items.text}</p>
           </div>
         ))}
