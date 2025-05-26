009cc0c6dcb4464b11fe8f374d4240167bc24742
Robin van Doorn <robinmvdoorn@gmail.com>
2025-05-26 14:41:31 +0200

    feature: add organisers

--- src/app/ui/OrganiserSection.jsx
+++ src/app/ui/OrganiserSection.jsx
@@ -1,17 +1,30 @@
-import React from "react";
-import { Card } from "primereact/card";
+"use client";
 
-export default function OrganiserSection({ organisers }) {
+import React from "react";
+import { Card } from 'primereact/card';
+
+export default function OrganiserSection({data}) {
   return (
-    <div className="card flex flex-wrap justify-content-center gap-3">
-      {organisers.map((organisers, index) => {
-        const header = <img alt="Card" src={organisers.header} />;
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-16">
+      {data.map((organiser) => {
+        const header = (
+          <img
+            alt="Card"
+            src={organiser.header}
+            className="w-full h-[156px] object-cover"
+          />
+        );
         return (
           <Card
-            key={index} 
-            title={organisers.title}
+            key={organiser.title}
+            pt={{body : "p-0"}}
+            title={
+              <span className="h-[55px] flex items-center justify-center w-full text-[15px] m-[5px]">
+                {organiser.title}
+              </span>
+            }
             header={header}
-            className="md:w-25rem"
+            className="w-[156px] h-[200px] mb-2"
           />
         );
       })}
