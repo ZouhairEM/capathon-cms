f5442bb750bb1d876f885c20699170ed8393e625
Beer <falco.de.beer@capgemini.com>
2025-05-27 15:00:04 +0200

    bug: resolved contentful issue

--- src/app/service/FetchData/FetchData.js
+++ src/app/service/FetchData/FetchData.js
@@ -3,7 +3,7 @@ const client = require('../InitializeClient/InitializeClient').default;
 export async function getData() {
   try {
     const entry = await client.getEntry(process.env.CONTENTFUL_ENTRY_ID);
-    return entry.fields.db.data;
+    return entry.fields.data.data;
   } catch (error) {
     console.error(error);
   }
