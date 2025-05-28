da3a1b1640eb57677ac6a0aa7746df760f503460
Robin van Doorn <robinmvdoorn@gmail.com>
2025-05-28 15:19:52 +0200

    wip

--- src/app/ui/OrganiserSection.jsx
+++ src/app/ui/OrganiserSection.jsx
@@ -1,33 +1,35 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Card } from "primereact/card";
+import React from 'react';
+import { Card } from 'primereact/card';
 
 export default function OrganiserSection({ data }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-16">
-      {data.map((organiser) => {
-        const header = (
-          <img
-            alt="Card"
-            src={organiser.header}
-            className="w-full h-[156px] object-cover"
-          />
-        );
-        return (
-          <Card
-            key={organiser.title}
-            pt={{ body: "p-0" }}
-            title={
-              <span className="h-[55px] flex items-center justify-center w-full text-[15px] m-[5px]">
-                {organiser.title}
-              </span>
-            }
-            header={header}
-            className="w-[156px] h-[200px] mb-2"
-          />
-        );
-      })}
+    <div className="mx-auto flex w-[900px] items-center justify-center">
+      <div className="grid grid-cols-1 justify-items-center gap-20 sm:grid-cols-2 md:grid-cols-3">
+        {data.map((organiser) => {
+          const header = (
+            <img
+              alt="Card"
+              src={organiser.header}
+              className="h-[155px] object-cover"
+            />
+          );
+          return (
+            <Card
+              key={organiser.title}
+              pt={{ body: 'p-0' }}
+              title={
+                <span className="m-[15px] flex items-center justify-center text-center font-sans text-[15px]">
+                  {organiser.title}
+                </span>
+              }
+              header={header}
+              className="mb-2 w-[155px]"
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
