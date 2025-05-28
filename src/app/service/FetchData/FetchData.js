de7aed34df3bde258367d3e08ca98ec9f843322e
Falco de Beer <81578892+falcocap@users.noreply.github.com>
2025-05-28 15:17:03 +0200

    Update src/app/service/FetchData/FetchData.js

--- src/app/service/FetchData/FetchData.js
+++ src/app/service/FetchData/FetchData.js
@@ -3,7 +3,7 @@ const client = require('../InitializeClient/InitializeClient').default;
 export async function getData() {
   try {
     const entry = await client.getEntry(process.env.CONTENTFUL_ENTRY_ID);
-    return entry.fields.data.data;
+    return entry.fields.db.data;
   } catch (error) {
     console.error(error);
   }
