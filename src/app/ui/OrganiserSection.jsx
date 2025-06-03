8a37eb2190f5ba5d459e98a1e365df65d28ee266
Robin van Doorn <robinmvdoorn@gmail.com>
2025-06-03 10:04:45 +0200

    fix: separate first and last name and added a line break for consistent styling for all names..

--- src/app/ui/OrganiserSection.jsx
+++ src/app/ui/OrganiserSection.jsx
@@ -5,27 +5,29 @@ import { Card } from 'primereact/card';
 
 export default function OrganiserSection({ data }) {
   return (
-    <div className="mx-auto flex w-[900px] items-center justify-center">
+    <div className="mx-auto flex w-[56rem] items-center justify-center">
       <div className="grid grid-cols-1 justify-items-center gap-20 sm:grid-cols-2 md:grid-cols-3">
-        {data.map((organiser) => {
+        {data.map((organisers) => {
           const header = (
             <img
               alt="Card"
-              src={organiser.header}
-              className="h-[155px] object-cover"
+              src={organisers.header}
+              className="h-[10rem] object-cover"
             />
           );
           return (
             <Card
-              key={organiser.title}
+              key={organisers.firstName}
               pt={{ body: 'p-0' }}
               title={
-                <span className="m-[15px] flex items-center justify-center text-center font-sans text-[15px]">
-                  {organiser.title}
+                <span className="m-[1rem] flex items-center justify-center text-center font-sans text-[1rem]">
+                  {organisers.firstName} 
+                  <br></br> 
+                  { organisers.lastName}
                 </span>
               }
               header={header}
-              className="mb-2 w-[155px]"
+              className="mb-2 w-[10rem]"
             />
           );
         })}
