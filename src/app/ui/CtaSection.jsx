108c27e678acb6d4a5746292317c01e5f55dd5d2
falcocap <falco.de.beer@capgemini.com>
2025-06-05 09:35:33 +0200

    feat: added decorative lines and made section responsive

--- src/app/ui/CtaSection.jsx
+++ src/app/ui/CtaSection.jsx
@@ -1,22 +1,41 @@
 'use client';
 
 export default function CtaSection({ data }) {
-  return (
-    <section>
-        <div className="flex flex-col items-end">
-            <div className="dingetje w-[1016px] mb-8">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s"></img>
-                <p className="ml-5">Do you have a specific business case, or a problem you want to share?</p>
+    return (
+        <section className="bg-capathon-primary h-256 mb-100">
+            <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl ml-10 mt-10">Show me what you have got</h1>
+            <div className="flex flex-col items-end lg:ml-10">
+                <div className="flex flex-row items-center mt-20 mb-8 w-full relative">
+                    <div className="relative flex-1">
+                        <div className="w-1 h-[807px] bg-black absolute left-0 top-0 hidden lg:block"></div>
+                        <div className="h-1 bg-black w-full"></div>
+                    </div>
+                    <div className="dingetje w-[80%] xl:w-[1016px] flex items-center opacity-70">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s" />
+                        <p className="ml-5">Do you have a specific business case, or a problem you want to share?</p>
+                    </div>
+                </div>
+                <div className="flex flex-row items-center mb-8 w-full lg:w-[calc(100%-64px)] relative">
+                    <div className="relative flex-1">
+                        <div className="w-1 h-[657px] bg-black absolute left-0 top-0 hidden lg:block"></div>
+                        <div className="h-1 bg-black w-full"></div>
+                    </div>
+                    <div className="dingetje w-[70%] xl:w-[754px] flex items-center opacity-70">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s" />
+                        <p className="ml-5">An interesting problem space?</p>
+                    </div>
+                </div>
+                <div className="flex flex-row items-center mb-8 w-full lg:w-[calc(100%-128px)] relative">
+                    <div className="relative flex-1">
+                        <div className="w-1 h-[507px] bg-black absolute left-0 top-0 hidden lg:block"></div>
+                        <div className="h-1 bg-black w-full"></div>
+                    </div>
+                    <div className="dingetje w-[60%] xl:w-[492px] flex items-center opacity-70">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s" />
+                        <p className="ml-5">Or just a cool idea?</p>
+                    </div>
+                </div>
             </div>
-            <div className="dingetje w-[754] mb-8">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s"></img>
-                <p className="ml-5">An interesting problem space?</p>
-            </div>
-            <div className="dingetje w-[492px]">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKaiZ2jc68FUsiaHg32uI1RAUHw673w4F-w&s"></img>
-                <p className="ml-5">Or just a cool idea?</p>
-            </div>
-        </div>
-    </section>
-  );
+        </section>
+    );
 }
