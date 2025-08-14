f47516f5c33ec41d8896850ddb81b24b91d1cbb4
falcocap <falco.de.beer@capgemini.com>
2025-08-14 10:55:43 +0200

    style: finalized styling for organizer cards

--- src/app/ui/OrganiserSection.jsx
+++ src/app/ui/OrganiserSection.jsx
@@ -7,8 +7,8 @@ export default function OrganiserSection({ data }) {
   return (
     <div className='bg-capathon-secondary bg-[url(/asfalt-light.png)]'>
       <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl ml-10 pt-10 pb-20 text-white">{data.title}</h1>
-      <div className=" mx-auto flex w-[56rem] items-center justify-center">
-        <div className="grid grid-cols-1 justify-items-center gap-20 sm:grid-cols-2 md:grid-cols-3 pb-20">
+      <div className="mx-auto flex items-center justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-25 pb-20">
           {data.organisers.map((organisers) => {
             const header = (
               <img
@@ -21,8 +21,9 @@ export default function OrganiserSection({ data }) {
               <Card
                 key={organisers.firstName}
                 pt={{
-                  body: 'p-0',
-                  title: 'm-0 text-black font-medium title-block cut-corners-tr-bl absolute',
+                  root: 'h-53',
+                  body: 'p-0 m-0 text-black title-block cut-corners-tr-bl absolute opacity-50 !h-20',
+                  title: 'top-0 p-0 h-13 content-center font-medium',
                 }}
                 title={
                   <span className="left[-15%] relative flex items-center justify-center text-center font-sans text-[1rem]">
